5f7aab6ab67b799da862afc31a3b70225ec551b9
gilofos <akatseras@gmail.com>
2026-02-07 13:42:09 +0200

    Update kairos.py

--- kairos.py
+++ kairos.py
@@ -13,12 +13,10 @@ def get_weather():
         data = response.json()
 
         if response.status_code == 200:
-            # Ρύθμιση ώρας Ελλάδας (UTC+2) - Χωρίς έξτρα βιβλιοθήκες
+            # Ώρα Ελλάδας (UTC+2)
             offset = timezone(timedelta(hours=2))
             current_time = datetime.now(offset).strftime("%H:%M:%S")
 
-            # Διόρθωση πίεσης για το υψόμετρο του Γηλόφου (1050μ)
-            # Προσθέτουμε 118 hPa για αναγωγή στη στάθμη της θάλασσας
             sea_level_pressure = data["main"]["pressure"] + 118
 
             weather_info = {
@@ -26,19 +24,17 @@ def get_weather():
                 "humidity": data["main"]["humidity"],
                 "pressure": sea_level_pressure,
                 "description": data["weather"][0]["description"].capitalize(),
-                "icon": data["weather"][0]["icon"],
                 "last_update": current_time
             }
 
             with open("data.json", "w", encoding="utf-8") as f:
                 json.dump(weather_info, f, ensure_ascii=False, indent=4)
             
-            print(f"Επιτυχής ενημέρωση: {current_time}")
+            print(f"Update Done: {current_time}")
         else:
-            print(f"Σφάλμα API: {data.get('message')}")
-
+            print("API Error")
     except Exception as e:
-        print(f"Σφάλμα συστήματος: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     get_weather()
